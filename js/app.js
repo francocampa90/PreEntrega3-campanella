fd468d96980f03670c7cc30ccdfc6eafef09c07a
Franco Campanella <francocampa90@gmail.com>
2023-06-10 15:45:50 -0300

    cambio

--- js/app.js
+++ js/app.js
@@ -151,37 +151,33 @@ function actualizarNumerito(){
 
 
 
-
 const monedaEl_one = document.getElementById('moneda-uno');
 const monedaEl_two = document.getElementById('moneda-dos');
 const cantidadEl_one = document.getElementById('cantidad-uno');
 const cantidadEl_two = document.getElementById('cantidad-dos');
-
 const cambioEl = document.getElementById('cambio');
-
 const tazaEl = document.getElementById('taza');
 
 
-
-
-
-
-async function calculate(){
+// Fetch Exchange Rate and Update the DOM
+function calculate(){
     const moneda_one = monedaEl_one.value;
     const moneda_two = monedaEl_two.value;
 
-
-
-    const cambioapi = await fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`);
-    const data = await cambioapi.json();
-
+   fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+   .then(res => res.json() )
+   .then(data => {
        const taza = data.rates[moneda_two];
+       
        cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
        cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
-   
+
+    } );
     
 }
 
+//Event listeners
 monedaEl_one.addEventListener('change', calculate);
 cantidadEl_one.addEventListener('input', calculate);
 monedaEl_two.addEventListener('change', calculate);
@@ -196,6 +192,3 @@ taza.addEventListener('click', () =>{
 
 
 calculate();
-
-
-
